87d053aad4aac02d84ec6a9fba37097fe1a648eb
Pormes <66345108+pormes@users.noreply.github.com>
2020-12-07 01:14:00 +0900

    Update unlimited.sh

--- unlimited.sh
+++ unlimited.sh
@@ -435,6 +435,7 @@ therock="232192182"
 kylie="12281817"
 jelopez="305701719"
 messi="427553890"
+anonymous_dikha_official="35355660673"
 
 dualipa="12331195"
 mileycyrus="325734299"
@@ -447,8 +448,6 @@ madonna="181306552"
 leonardodicaprio="1506607755"
 ladygaga="184692323"
 taylorswift="11830955"
-instagram="25025320"
-
 
 if [[ ! -e celeb_id ]]; then
 printf "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" $dualipa $mileycyrus $shawnmendes $katyperry $charlieputh $lelepons $camila_cabello $madonna $leonardodicaprio $ladygaga $taylorswift $instagram $neymar $selena $ariana $beyonce $cristiano $kimkardashian $kendall $therock $kylie $jelopez $messi > celeb_id
